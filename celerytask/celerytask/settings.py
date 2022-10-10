0691467cc1a7de5a249485dc2995b234ac021f74
Aradhya N Sasi <aradhya.n.sasi101@gmail.com>
2022-10-11 00:06:55 +0530

    change db to postgres

--- celerytask/celerytask/settings.py
+++ celerytask/celerytask/settings.py
@@ -74,11 +74,22 @@ WSGI_APPLICATION = 'celerytask.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': str(BASE_DIR / "db.sqlite3"),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / "db.sqlite3"),
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'turbolabs',
+        'USER': 'turbolabs_user',
+        'PASSWORD': 'mypass',
+        'HOST': 'localhost',
+        'PORT': '',
+   }
 }
 
 
