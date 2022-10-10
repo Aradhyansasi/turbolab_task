b8033eb12aa54434b878ae9d0064e79b77fbd131
Aradhya N Sasi <aradhya.n.sasi101@gmail.com>
2022-10-10 22:30:09 +0530

    Remove unwatned logs

--- celerytask/celerytask/tasks.py
+++ celerytask/celerytask/tasks.py
@@ -13,7 +13,6 @@ from celerytask.celery import app
 
 @app.task()
 def generate_csv(name, total):
-    print('Im being called')
     with open('./generate_file/data/'+name+'.csv', 'w', newline='') as csvfile:
         spamwriter = csv.writer(csvfile, delimiter=' ',quotechar='|', quoting=csv.QUOTE_MINIMAL)
         for i in range(0, int(total)):
