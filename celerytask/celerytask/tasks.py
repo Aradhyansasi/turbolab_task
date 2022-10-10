c128ee756251a79f0532d41d45bd34253e11be39
Aradhya N Sasi <aradhya.n.sasi101@gmail.com>
2022-10-10 22:32:41 +0530

    Remove unwanted comments

--- celerytask/celerytask/tasks.py
+++ celerytask/celerytask/tasks.py
@@ -1,15 +1,9 @@
-from mimetypes import init
 import random
 import string
 import random
 import csv
 from celerytask.celery import app
-# from celery import Celery
 
-# app = Celery('celerytask', broker='amqp://myuser:mypassword@localhost:5672/myvhost')
-
-# def debug_task(self):
-#     print('Request: {0!r}'.format(self.request))
 
 @app.task()
 def generate_csv(name, total):
